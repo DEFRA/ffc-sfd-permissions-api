ca9beb18b2f1a93936fcafb9ccce2062472f8129
Fay Toward <fay.toward@defra.gov.uk>
2024-03-11 16:37:50 +0000

    Update preference routes and docker-compose ports

--- app/routes/preference.js
+++ app/routes/preference.js
@@ -1,33 +1,16 @@
-const { PreferenceType } = require("../graphql/preference")
-
-module.exports = [ {
-    method: 'GET',
-    path: '/preference',
-    config: {
-      tags: ['graphql'],
-      handler: (request, h) => {
-        return h.response(PreferenceType).code(200)
-      }
-    }
-  },
+module.exports = [ 
   {
     method: 'GET',
     path: '/preference/{sbi}',
-    config: {
-      tags: ['graphql'],
       handler: (request, h) => {
-        return h.response(PreferenceType).code(200)
+        return h.response("ok").code(200)
       }
-    }
   },
   {
     method: 'POST',
     path: '/preference',
-    config: {
-      tags: ['graphql'],
       handler: (request, h) => {
-        return h.response(PreferenceType).code(200)
+        return h.response("working").code(200)
       }
     }
-  }
 ]
