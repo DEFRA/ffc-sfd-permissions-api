47d6987d02f44726be405613f1bdee75754e9eeb
Fay Toward <fay.toward@defra.gov.uk>
2024-03-19 09:25:24 +0000

    Update Dockerfile and database configuration

--- app/models/preferences.js
+++ app/models/preferences.js
@@ -1,11 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('preferences', {
-      sbi: { type: DataTypes.INTEGER, primaryKey: true},
-      preferenceType: DataTypes.STRING,
-    },
-    {
-      tableName: 'preferences',
-      freezeTableName: true,
-      timestamps: false
-    })
-  }
+  return sequelize.define('preferences', {
+    sbi: { type: DataTypes.INTEGER, primaryKey: true },
+    preferenceType: DataTypes.STRING
+  },
+  {
+    tableName: 'preferences',
+    freezeTableName: true,
+    timestamps: false
+  })
+}
--- app/routes/preference.js
+++ app/routes/preference.js
@@ -1,18 +1,18 @@
 const db = require('../data')
 
-module.exports = [ 
+module.exports = [
   {
     method: 'GET',
     path: '/preference/{sbi}',
-      handler: async (request, h) => {
-        return h.response(await db.Preference.findOne({ where: { sbi: request.query.sbi } }))
-      }
+    handler: async (request, h) => {
+      return h.response(await db.Preference.findOne({ where: { sbi: request.query.sbi } }))
+    }
   },
   {
     method: 'POST',
     path: '/preference',
-      handler: async (request, h) => {
-        return h.response(await db.Preference.create(request.payload))
-      }
+    handler: async (request, h) => {
+      return h.response(await db.Preference.create(request.payload))
     }
+  }
 ]
